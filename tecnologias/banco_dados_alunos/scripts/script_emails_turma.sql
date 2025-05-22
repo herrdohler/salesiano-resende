3c24736a56079466d49b1197d199f6827b8dcd8c
Arthur Döhler <arthurdmf@hotmail.com>
2025-05-22 14:37:30 -0300

    Update script_emails_turma.sql

--- tecnologias/banco_dados_alunos/scripts/script_emails_turma.sql
+++ tecnologias/banco_dados_alunos/scripts/script_emails_turma.sql
@@ -4,6 +4,7 @@ FROM dados_alunos
 GROUP BY turma;
 
 -- Seleção de e-mails de uma única turma específica
-SELECT turma, GROUP_CONCAT(e_mail, '; ') AS lista_emails 
+SELECT turma, GROUP_CONCAT(e_mail, '; ') AS lista_emails
 FROM dados_alunos
-WHERE turma = '6ano_A';
+WHERE turma = '6ano_A'
+GROUP BY turma;
