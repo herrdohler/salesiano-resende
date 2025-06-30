9ef9dc449186f07150ed81ac72a45ffc4f821a89
Arthur Döhler <arthurdmf@hotmail.com>
2025-06-30 16:45:41 -0300

    Update estrutura_tabela.sql

--- tecnologias/banco_dados_alunos/arquitetura/estrutura_tabela.sql
+++ tecnologias/banco_dados_alunos/arquitetura/estrutura_tabela.sql
@@ -1,6 +1,9 @@
 -- Estrutura de criação da tabela principal que armazenará os dados dos alunos
+
 CREATE TABLE dados_alunos (
+    
     -- Número de matrícula único de cada aluno
+    
   `matricula` varchar(20) NOT NULL,
   `nome_completo` varchar(255) DEFAULT NULL,
   `e_mail` varchar(255) DEFAULT NULL,
