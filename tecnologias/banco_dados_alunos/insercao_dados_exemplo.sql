96c413b1d267b8a1c52640077739d2421b77a2d8
Arthur Döhler <arthurdmf@hotmail.com>
2025-06-30 16:42:04 -0300

    Delete tecnologias/banco_dados_alunos/insercao_dados_exemplo.sql

--- tecnologias/banco_dados_alunos/insercao_dados_exemplo.sql
+++
@@ -1,5 +0,0 @@
--- Inserção de dados fictícios
-INSERT INTO dados_alunos (matricula, nome_completo, e_mail, turma) VALUES
-(123456789, 'JOÃO SILVA', 'joao.silva@escola.com', '1ano_A'),
-(987654321, 'MARIA OLIVEIRA', 'maria.oliveira@escola.com', '2ano_B'),
-(456789123, 'CARLOS PEREIRA', 'carlos.pereira@escola.com', '3ano_C');
